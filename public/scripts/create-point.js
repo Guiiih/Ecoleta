32e50c0cc22b7b19ccc779666658ee071104a013
Guilherme Nascimento <34890049+Guiiih@users.noreply.github.com>
2020-06-05 14:01:57 -0300

    Update create-point.js

--- public/scripts/create-point.js
+++ public/scripts/create-point.js
@@ -65,6 +65,8 @@ function handleSelectedItem(event) {
 
     const itemId = itemLi.dataset.id
 
+    // console.log('ITEM ID: ', itemId)
+
     // verificar se existem items selecionados, se sim
     // pegar os items selecionados
 
@@ -88,6 +90,8 @@ function handleSelectedItem(event) {
         selectedItems.push(itemId)
 
     }
+
+    //console.log('selectedItems: ', selectedItems)
 
     //atualizar o campo escondido com os itens selecionado
     collectedItems.value = selectedItems
